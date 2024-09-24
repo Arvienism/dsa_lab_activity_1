14fa195dd4075bbe08f8558763367f01b8df83ee
Arvie Lastra <lastraarvie@gmail.com>
2024-09-24 22:43:58 +0800

    Added algorithm for converting temperature

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,7 +1,33 @@
-'''1.	Ask the user to input a temperature.
-2.	Ask the user to select the conversion type: from Celsius to Fahrenheit or from Fahrenheit to Celsius.
-3.	Perform the appropriate conversion and print the result.
-'''
 # create a method for converting celsius to fahrenheit and fahrenheit to celsius.
+class temperature_converter:
+    def __init__(self, temperature) -> None:
+        self.temperature = temperature
+
+    def celsius_to_fahrenheit(self):
+        return (self.temperature * 1.8) + 32
+    
+    def fahrenheit_to_celsius(self):
+        return (self.temperature - 32) * 1/1.8
 # ask user to input the a temperature. 
+class main:
+    while True:
+        temp = float(input("Enter the temperature: "))
+        conversion_type = str(input('\n' +
+                            'Select conversion type: \n' +
+                            'if Celsius to Fahrenheit, type: "1" \n' +
+                            'if Fahrenheit to Celsius, type: "2" \n' + '\n'
+                            'Type your conversion type number: ')).strip()
+
+        temp_converter = temperature_converter(temp) 
 # print the results.
+        if conversion_type == "1":
+            result = temp_converter.celsius_to_fahrenheit()
+            print(f"{temp}°C is {result:.2f}°F")
+            break
+        elif conversion_type == "2":
+            result = temp_converter.fahrenheit_to_celsius()
+            print(f"{temp}°F is {result:.2f}°C")
+            break
+        else:
+            print('"INVALID! Please, select the correct conversion type number." \n' + '\n')
+main()
