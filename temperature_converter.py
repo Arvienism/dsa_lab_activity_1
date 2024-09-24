e4f251dce461bc463647c56a5b0cabf8e8bc02a8
Arvie Lastra <lastraarvie@gmail.com>
2024-09-24 18:49:11 +0800

    added pseudocode

---
+++ temperature_converter.py
@@ -0,0 +1,7 @@
+'''1.	Ask the user to input a temperature.
+2.	Ask the user to select the conversion type: from Celsius to Fahrenheit or from Fahrenheit to Celsius.
+3.	Perform the appropriate conversion and print the result.
+'''
+# create a method for converting celsius to fahrenheit and fahrenheit to celsius.
+# ask user to input the a temperature. 
+# print the results.
